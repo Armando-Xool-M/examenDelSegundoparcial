043529fd9ef4633f396db0dba07e380c070d872e
Armando-Xool-M <94819656+Armando-Xool-M@users.noreply.github.com>
2023-05-09 14:56:31 -0600

    Avancee

---
+++ Examen-planetas/src/main/java/com/example/demo/modelo/planetasDTO.java
@@ -0,0 +1,34 @@
+package com.example.demo.modelo;
+
+public class planetasDTO {
+    private double UA;
+    private int id;
+    private double milesk;
+    private String nombre;
+	public double getUA() {
+		return UA;
+	}
+	public void setUA(double uA) {
+		UA = uA;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getMilesk() {
+		return milesk;
+	}
+	public void setMilesk(double milesk) {
+		this.milesk = milesk;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+    
+    
+}
